5e9531dc157ca0861c19e7da94849037959ec19e
nexustix <nexus1768@tuta.io>
2016-08-31 22:13:26 +0200

    Remove orecontrol cobwebs

---
+++ Adamantine/nxreplicator/addAtoms.sh
@@ -0,0 +1,46 @@
+packName=Adamantine
+
+#TODO delete molecule first
+
+nxfusion add $packName curse actually-additions
+nxfusion add $packName curse chameleon
+nxfusion add $packName curse charset
+nxfusion add $packName curse correlated-potentialistics
+nxfusion add $packName curse ender-io
+nxfusion add $packName curse endercore
+nxfusion add $packName curse enderthing
+nxfusion add $packName curse extra-utilities
+nxfusion add $packName curse ftb-utilities
+#dependency (ftblib)
+nxfusion add $packName curse giacomos-foundry
+nxfusion add $packName curse industrial-craft
+nxfusion add $packName curse inventory-tweaks
+nxfusion add $packName curse journeymap-32274
+nxfusion add $packName curse just-enough-items-jei
+nxfusion add $packName curse just-enough-resources-jer
+nxfusion add $packName curse openglasses
+nxfusion add $packName curse opencomputers
+#nxfusion add $packName curse openfm
+nxfusion add $packName curse openprinter
+nxfusion add $packName curse openradio
+nxfusion add $packName curse ore-control
+nxfusion add $packName curse redstone-paste
+nxfusion add $packName curse rftools
+nxfusion add $packName curse rftools-dimensions
+nxfusion add $packName curse roguelike-dungeons
+nxfusion add $packName curse simpleoregen
+nxfusion add $packName curse storage-drawers
+nxfusion add $packName curse the-one-probe
+nxfusion add $packName curse tis-3d
+nxfusion add $packName curse vending-block
+nxfusion add $packName curse water-strainer
+
+nxfusion add $packName curse immersive-engineering
+
+
+nxfusion add $packName custom adm-ic2-cfg
+nxfusion add $packName custom adm-simpleoregen-cfg
+nxfusion add $packName custom adm-wstrain-cfg
+nxfusion add $packName custom adm-wstrain-loot-cfg
+
+#nxcrunch generate $packName $packName
